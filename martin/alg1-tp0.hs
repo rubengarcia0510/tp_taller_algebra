7ebfc1edf534a95e88deaf1063d9929b5d28fc83
Garcia <ruben.garcia@atos.net>
2019-06-07 00:41:31 -0300

    primer commit tp

--- martin/alg1-tp0.hs
+++ martin/alg1-tp0.hs
@@ -4,7 +4,6 @@ type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
 type Publicacion = (Usuario, String, Set Usuario) -- (usuario que publica, texto publicacion, likes)
 type RedSocial = (Set Usuario, Set Relacion, Set Publicacion)
 
-
 -- Funciones basicas
 
 usuarios :: RedSocial -> Set Usuario
@@ -17,10 +16,10 @@ publicaciones :: RedSocial -> Set Publicacion
 publicaciones (_, _, ps) = ps
 
 idDeUsuario :: Usuario -> Integer
-idDeUsuario (id, _) = id
+idDeUsuario (id, _) = id 
 
 nombreDeUsuario :: Usuario -> String
-nombreDeUsuario (_, nombre) = nombre
+nombreDeUsuario (_, nombre) = nombre 
 
 usuarioDePublicacion :: Publicacion -> Usuario
 usuarioDePublicacion (u, _, _) = u
