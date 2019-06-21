3dbbfb7dcfa450dd4b3c8077227b3a561c95f223
Garcia <ruben.garcia@atos.net>
2019-06-21 00:16:36 -0300

    primerMultiplode45345

---
+++ ruben/taller_algebra/clase7.hs
@@ -0,0 +1,15 @@
+listar :: a -> a -> a -> [a]
+listar a b c = [a,b,c]
+
+pertenece :: Integer -> [Integer] -> Bool
+pertenece e [] = False
+pertenece e l | e == head l = True
+              | otherwise = pertenece e (tail l)
+
+lista_decreciente :: Integer -> [Integer] -> [Integer]
+lista_decreciente (-100) l = ((-100):l)
+lista_decreciente n l = (n - 1:lista_decreciente (n-1) l)
+
+primerMultiplode45345 :: [Integer] -> Integer
+primerMultiplode45345 (l:ls) | l > 45345 && mod l 45345 == 0 = l
+                             | otherwise = primerMultiplode45345 ls
